f6ba225beae2de0698083f16a2d33e46c32aff55
Mike <migueljrojas@gmail.com>
2018-10-01 16:49:17 -0300

    Fixes mobile

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -98,25 +98,26 @@ var Header = function() {
         return urlparameter;
     }
 
-    var hideIntro = getUrlParam('hideIntro', false);
+    var hideIntro = sessionStorage.getItem('chakana');;
+    var canvas = document.getElementById('canvas');
+    var clientWidth = document.documentElement.clientWidth;
+    var clientHeight = document.documentElement.clientHeight;
 
     if (!hideIntro) {
         var url;
-        var clientWidth = document.documentElement.clientWidth;
-        var clientHeight = document.documentElement.clientHeight;
+
         if (clientWidth < 700) {
             url = '/images/home/intro-mobile.jpg';
         } else {
             url = '/images/home/intro.jpg';
         }
-        var canvas = document.getElementById('canvas');
+
         var ctx = canvas.getContext('2d');
         var img = new Image();
         img.src = url;
         img.onload = function () {
           var width = clientWidth;
           var height = clientHeight;
-
           canvas.width = width;
           canvas.height = height;
           ctx.drawImage(img, 0, 0, width, height);
@@ -125,9 +126,12 @@ var Header = function() {
         var isPress = false;
         var old = null;
         var count = 0;
-        canvas.addEventListener('mousedown', function (e){
-          isPress = true;
-          old = {x: e.offsetX, y: e.offsetY};
+        canvas.addEventListener('mouseenter', function (e){
+          setTimeout(function() {
+              isPress = true;
+              old = {x: e.offsetX, y: e.offsetY};
+          }, 3000);
+
         });
         canvas.addEventListener('touchstart', function (e){
           isPress = true;
@@ -141,7 +145,7 @@ var Header = function() {
           erase(e);
         });
 
-        canvas.addEventListener('mouseup', function (e){
+        canvas.addEventListener('mouseleave', function (e){
           isPress = false;
         });
         canvas.addEventListener('touchend', function (e){
@@ -149,7 +153,6 @@ var Header = function() {
         });
 
         function erase(e) {
-            console.log(e);
             if (isPress) {
               var x;
               var y;
@@ -165,8 +168,13 @@ var Header = function() {
               ctx.globalCompositeOperation = 'destination-out';
 
               ctx.beginPath();
-              ctx.arc(x, y, 50, 0, 2 * Math.PI);
-              ctx.fillStyle = "rgba(0,0,0,.1)";
+              //ctx.arc(x, y, 150, 0, 2 * Math.PI);
+              if (clientWidth < 700) {
+                  ctx.arc(x, y, 50, 0, 2 * Math.PI);
+              } else {
+                  ctx.arc(x, y, 150, 0, 2 * Math.PI);
+              }
+              //ctx.fillStyle = "rgba(0,0,0,.1)";
               ctx.fill();
 
               ctx.lineWidth = 20;
@@ -182,12 +190,14 @@ var Header = function() {
         }
 
         function checkCount() {
-            console.log(count);
-            if (count >= 300) {
+            if (count >= 250) {
                 body.removeClass('-hideOverflow');
                 canvas.classList.add('-hidden');
+                sessionStorage.setItem('chakana', true);
                 setTimeout(function() {
-                    canvas.parentNode.removeChild(canvas);
+                    if (canvas.parentNode) {
+                        canvas.parentNode.removeChild(canvas);
+                    }
                 }, 500);
             } else {
                 return false;
@@ -201,8 +211,69 @@ var Header = function() {
     } else {
         setTimeout(function() {
             $('.home').removeClass('-hidden');
+            canvas.parentNode.removeChild(canvas);
         }, 500);
     }
+
+    var sections = $('section');
+    var navItems = $('.home__nav li');
+    var visible;
+
+    function isVisible(el) {
+        var top = el.get(0).getBoundingClientRect().top;
+
+        if ( top <= 100 && top >= 0 ) {
+            visible = true;
+        } else {
+            visible = false;
+        }
+
+        return visible;
+    }
+
+    function scrollCheck(el) {
+        el.each(function(){
+            var $this = $(this);
+
+            $(window).on('scroll', function(){
+                _.debounce(isVisible($this), 300);
+
+                if (visible === true) {
+                    checkSection($this);
+                }
+            });
+        });
+    }
+
+    function setActiveNavItem() {
+        var sectionsArray = $.makeArray(sections);
+        var activeSectionINdex = sectionsArray.findIndex(function(el) {
+            return el.className.includes('-visible');
+        });
+
+        navItems.removeClass('-active');
+
+        $(navItems.get(activeSectionINdex)).addClass('-active');
+    }
+
+    function checkSection(el) {
+        var activeSection = $('section.-visible');
+
+        if (!el.hasClass('-visible')) {
+            el.addClass('-visible');
+            setActiveNavItem();
+        } else {
+            isVisible(activeSection);
+
+            if (visible === false) {
+                sections.removeClass('-visible');
+                return false;
+                scrollCheck(sections);
+            }
+        }
+    }
+
+    scrollCheck(sections);
 };
 
 module.exports = Header;
